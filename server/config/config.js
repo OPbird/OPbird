286b0fc6cc8b816365b2fd7528bc69ee5c9b645a
naxsel <asolanas93@gmail.com>
2016-05-06 13:22:27 +0200

    config mongo

--- server/config/config.js
+++ server/config/config.js
@@ -1,6 +1,6 @@
 module.exports = {
     db: {
-        //production:"mongodb://Phyrion:123456@ds059634.mongolab.com:59634/ophoras",
+        production:"mongodb://admin:admin@ds011472.mlab.com:11472/stw",
         test:"mongodb://localhost:27017"
     },
     TOKEN_SECRET: process.env.TOKEN_SECRET || "clavePrivadaToOp",
