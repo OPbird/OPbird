220ace1a67e753a04328066b7cc43cb7a5c4f184
naxsel <asolanas93@gmail.com>
2016-05-17 17:12:03 +0200

    angular

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,22 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'MainService']);
+angular.module("FinalApp",["ngRoute"])
+    .config(function($routeProvider) {
+        $routeProvider
+            .when("/", {
+                controller: "mainController",
+                templateUrl: "views/home.html"
+            })
+            .when("/home", {
+                controller: "mainController",
+                templateUrl: "views/home.html"
+            })
+            .when("/login",{
+                controller: "loginController",
+                templateUrl: "views/login.html"
+            })
+            .when("/register", {
+                controller: "registerController",
+                templateUrl: "views/register.html"
+            })
+            .otherwise({redirectTo: "/"});
+
+    })
