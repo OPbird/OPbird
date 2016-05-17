2d05d4fe340d25a8b018797810ea0ae09363d54b
naxsel <asolanas93@gmail.com>
2016-05-17 15:44:46 +0200

    controllers

--- public/app/app.js
+++ public/app/app.js
@@ -2,11 +2,20 @@ angular.module("FinalApp",["ngRoute"])
     .config(function($routeProvider) {
         $routeProvider
             .when("/", {
-                controller: "loginController",
-                templateUrl: "views/login.html"
+                controller: "mainController",
+                templateUrl: "views/home.html"
+            })
+            .when("/home", {
+                controller: "mainController",
+                templateUrl: "views/home.html"
+            })
+            .when("/login",{
+                controller: "loginContrfoller",
+                templateUrl: "view/login.html"
             })
             .when("/register", {
                 controller: "registerController",
                 templateUrl: "views/register.html"
             })
+
     })
