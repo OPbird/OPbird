47346e9bbee24ac2ff737cfc04d194b0cb0acebf
Daniel Uroz <uzetta27@hotmail.com>
2016-05-19 12:54:18 +0200

    Otro intento de estadisticas

--- public/js/controllers/adminStatsCtrl.js
+++ public/js/controllers/adminStatsCtrl.js
@@ -10,8 +10,8 @@ angular.module("FinalApp")
         }).then(function (response) {
             if (response.error > 0) {
             } else {
-                $scope.pieLabels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-                $scope.pieData = [300, 500, 100];
+                $scope.pieLabels = ["Altas", "Bajas"];
+                $scope.pieData = [response.data.ups, response.data.downs];
             }
         });
         //Metodo GET
@@ -21,8 +21,9 @@ angular.module("FinalApp")
         }).then(function (response) {
             if (response.error > 0) {
             } else {
-                $scope.doughnutLabels = ["Download Sales", "In-Store Sales", "Mail-Order Sales"];
-                $scope.doughnutData = [300, 500, 100];
+                $scope.doughnutLabels = ["Hoy", "3 días", "1 semanas", "1 mes", "Más de 1 mes"];
+                $scope.doughnutData = [response.data.todayAccess, response.data.threeDaysAccess, response.data.weekAccess,
+                    response.data.monthAccess, response.data.beyondMonth];
             }
         })
     });
