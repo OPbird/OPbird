75de3f805a39a6eae62b0ebccb77711c84e82f72
phyrion <rgcmb@hotmail.com>
2016-05-21 11:29:39 +0200

    Subiendo comparibilidad twitter con heroku 6 y definitiva

--- server/config/twitterConnection.js
+++ server/config/twitterConnection.js
@@ -11,8 +11,8 @@
 module.exports = {
     uri_request_token: "https://api.twitter.com/oauth/request_token",
     uri_access_token: "https://api.twitter.com/oauth/access_token",
-    consumer_key: "pYGaMnp0f5HroAUdxNyZhhZLT",
-    consumer_secret_key: "LSjwvq2AJnec2OKLylIM0v5VS4ql4B4yDONnz3K0Er0VETqrax",
+    consumer_key:  "EObdyi9bcvtMFcYD4a6chuxiB" || "pYGaMnp0f5HroAUdxNyZhhZLT",
+    consumer_secret_key: "GZDQNcMFJAci1y4xHWGxJDAFoq8262jQNsFwjwj3uheOAqCcf4" || "LSjwvq2AJnec2OKLylIM0v5VS4ql4B4yDONnz3K0Er0VETqrax",
     oauth_v: "1.0",
     uri_callback: "http://localhost:8080/auth/twitter/callback",
     signature: "HMAC-SHA1",
